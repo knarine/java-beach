d2b3fa2e07304a086360fc8017e1335c3c9e223e
Kalisha Narine <kalisha@medullan.com>
2014-06-05 16:48:00 -0400

    update comments

--- src/test/java/com/javahash/spring/controller/HelloControllerTest.java
+++ src/test/java/com/javahash/spring/controller/HelloControllerTest.java
@@ -13,6 +13,7 @@ import static com.jayway.restassured.RestAssured.given;
 
 public class HelloControllerTest {
 	
+	//Todo: make this work
 	/*@Before public void 
 	given_rest_assured_is_configured_with_greeting_controller() {
 	    RestAssuredMockMvc.standaloneSetup(new HelloWorldController());   
