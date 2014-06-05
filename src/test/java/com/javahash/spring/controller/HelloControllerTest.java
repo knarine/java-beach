5ae0aa6efe4ebc0ab06aa15560f0c02ee5231dc5
Kalisha Narine <kalisha@medullan.com>
2014-06-05 16:43:51 -0400

    adding unit test

---
+++ src/test/java/com/javahash/spring/controller/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.javahash.spring.controller;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.AssertionErrors;
+
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static com.jayway.restassured.RestAssured.given;
+
+public class HelloControllerTest {
+	
+	/*@Before public void 
+	given_rest_assured_is_configured_with_greeting_controller() {
+	    RestAssuredMockMvc.standaloneSetup(new HelloWorldController());   
+	}
+
+	@Test
+	public void hello_world_resource_returns_expected_greeting_in_body(){
+		given().
+        	when().
+        get("/hello").
+        	then().                 
+        body("content", equalTo("Hello : Kalisha"));
+	}*/
+	
+	@Test
+	public void very_simple_unit_test(){
+		String result = "one" + "two";
+		assertEquals("onetwo", result);
+	}
+
+}
