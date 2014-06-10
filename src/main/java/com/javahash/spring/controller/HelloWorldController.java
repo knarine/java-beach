41a29e8145ec02da065bb9b9ea9667ebca6d94fb
Kalisha Narine <kalisha@medullan.com>
2014-06-10 18:03:57 -0400

    change view text

--- src/main/java/com/javahash/spring/controller/HelloWorldController.java
+++ src/main/java/com/javahash/spring/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorldController { 
 
     @RequestMapping("/hello")
-    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String hello(@RequestParam(value="name", required=false, defaultValue="User") String name, Model model) {
         model.addAttribute("name", name);
         return "helloworld";
         //comments for changeset
